6e8b3a1d5551d074d854847931438abc5c232282
phamvietloi <43338839+phamvietloi@users.noreply.github.com>
2020-06-02 02:12:07 +0700

    Update auto_squid.sh

--- auto_squid.sh
+++ auto_squid.sh
@@ -1,27 +1,25 @@
 #!/bin/bash
 
-#felipesi - 2017
-
 echo "UPDATING SYSTEM, WAIT..."
 apt-get update -y > /dev/null
 apt-get upgrade -y > /dev/null
 echo -e "UPDATE SYSTEM - [OK]\n"
 
 echo "INSTALLING SERVER PROXY SQUID..."
-apt-get install squid3 -y > /dev/null
+apt-get install squid -y > /dev/null
 echo -e "INSTALL SQUID - [OK]\n"
 
 echo "INSTALLING CURL..."
 apt-get install curl -y > /dev/null
 echo -e "INSTALL CURL - [OK]\n"
 
-cd /etc/squid3
+cd /etc/squid
 
 ip=$(curl -s icanhazip.com) > /dev/null
 
 echo "SETTING UP SQUID..."
 
-echo "http_port 8080" > squid.conf
+echo "http_port 8888" > squid.conf
 echo "acl net dstdomain $ip" >> squid.conf
 echo "acl REQUEST method GET" >> squid.conf
 echo "acl REQUEST method POST" >> squid.conf
@@ -36,7 +34,7 @@ echo "acl liberado url_regex -i" >> squid.conf
 echo -e "SETTING UP SQUID - [OK]\n"
 
 echo "RESTARTING SERVICES..."
-service squid3 restart
+service squid restart
 echo -e "RESTART SERVICES - [OK]\n"
 
-echo "YOUR PROXY SERVER IS READY - IP: $ip PORT: 8080"
+echo "YOUR PROXY SERVER IS READY - IP: $ip PORT: 8888"
